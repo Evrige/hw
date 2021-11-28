56bb0afeeded16ef989a737a3b3b90b75c1a6757
Artur Dudnik <evrige.game@gmail.com>
2021-11-28 20:34:12 +0200

    updating logic

--- hw3/script.js
+++ hw3/script.js
@@ -1,4 +1,10 @@
-const firstNum = +prompt("Введите первое число"),
-	secondNum = +prompt("Введите второе число"),
-	result = firstNum + secondNum;
+let firstNum, secondNum;
+
+firstNum = +prompt("Введите первое число");
+secondNum = +prompt("Введите второе число");
+while (isNaN(firstNum) || firstNum == "" || firstNum == null) firstNum = +prompt("Введите первое число");
+while (isNaN(secondNum) || secondNum == "" || secondNum == null) secondNum = +prompt("Введите второе число");
+
+const result = firstNum + secondNum;
+console.log(firstNum, secondNum);
 alert(`Сумма чисел: ${firstNum} и ${secondNum} равна = ${result}`);
