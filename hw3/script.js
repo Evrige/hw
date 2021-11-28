f728e21ec977f8c07c62f4ee34b811b107cc1426
Artur Dudnik <evrige.game@gmail.com>
2021-11-28 20:13:01 +0200

    third homework

---
+++ hw3/script.js
@@ -0,0 +1,4 @@
+const firstNum = +prompt("Введите первое число"),
+	secondNum = +prompt("Введите второе число"),
+	result = firstNum + secondNum;
+alert(`Сумма чисел: ${firstNum} и ${secondNum} равна = ${result}`);
